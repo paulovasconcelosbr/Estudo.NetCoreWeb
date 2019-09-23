139bd64afcb2236f37908e45f7f1a7d105adfd53
paulo <paulovasconcelosbr@gmail.com>
2019-09-22 22:37:25 -0300

    subindo primeira alteração curso alura

---
+++ Estudo.NetCoreWeb/Catalogo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Estudo.NetCoreWeb
+{
+    public class Catalogo
+    {
+        public List <Livro> GetLivros()
+        {
+            var livros = new List<Livro>();
+            livros.Add(new Livro("001", "Quem mexeu na minha Query", 12.99m));
+            livros.Add(new Livro("002", "Fique Rico com C#", 30.99m));
+            livros.Add(new Livro("003", "Java para Baixinho", 25.99m));
+            return livros;
+        }
+    }
+}
